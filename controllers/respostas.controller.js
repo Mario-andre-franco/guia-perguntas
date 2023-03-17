71d2d36e52418460374c9778a2f6025ea9f1d4db
Mario Franco <mario.franco@wdev.int>
2023-03-17 09:56:06 -0300

    ajustando bug de responder pergunta por Id

--- controllers/respostas.controller.js
+++ controllers/respostas.controller.js
@@ -3,10 +3,11 @@ const Resposta = require('../model/resposta.model')
 
 exports.perguntaRespondida = async (req,res) => {
     const {corpo,perguntaId} = req.body
+    console.log(perguntaId);
     Resposta.create({
         corpo:corpo,
         perguntaId:perguntaId
     }).then(() => {
-        res.status(201).redirect("/")
+        res.redirect("/pergunta/"+perguntaId)
     })  
 }
